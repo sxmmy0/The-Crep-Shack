e08470d0f92878176f308c86e5bbec58f0a5c4d0
samobembe <sam.obembe@datacenterdynamics.com>
2023-10-25 15:59:37 +0100

    made new migrations

--- The_Crep_Shack/website/models.py
+++ The_Crep_Shack/website/models.py
@@ -9,6 +9,8 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+
+# Customer details
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -19,10 +21,26 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
+#All of our Products
 class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
 
     def __str__(self):
-        return self.title
+        return self.name
     
+# All of the Customer Orders
 class Order(models.Model):
-    pass
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
