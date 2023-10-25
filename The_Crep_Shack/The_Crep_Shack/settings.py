6a84940320c26443efb1c0b621eb03577f62a12a
samobembe <sam.obembe@datacenterdynamics.com>
2023-10-25 11:52:54 +0100

    added models, urls, setting and styling

--- The_Crep_Shack/The_Crep_Shack/settings.py
+++ The_Crep_Shack/The_Crep_Shack/settings.py
@@ -139,7 +139,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # Add the account middleware:
     'allauth.account.middleware.AccountMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
--- The_Crep_Shack/website/urls.py
+++ The_Crep_Shack/website/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import item_list
+
+app_name = 'website'
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('', item_list, name='item-list')
 ]
