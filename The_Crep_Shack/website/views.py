65997b4bf197d963b5e22021735586676f526c28
samobembe <sam.obembe@datacenterdynamics.com>
2023-10-27 13:29:14 +0100

    created login page and made new urls

--- The_Crep_Shack/website/views.py
+++ The_Crep_Shack/website/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Category, Order, Product, Customer
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     products = Product.objects.all()
     return render(request, "content/home.html", {'products':products})
 
+def about(request):
+    return render(request, "content/about.html", {})
+
+def login_user(request):
+    return render(request, "content/login.html", {})
+
+def logout_user(request):
+    pass
