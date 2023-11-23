447dcbf13ee330f1ad1d69714248eb1a47c614e0
samobembe <sam.obembe@datacenterdynamics.com>
2023-11-23 10:23:51 +0000

    updated context processors.py and cart.py

--- The_Crep_Shack/shopping_cart/cart.py
+++ The_Crep_Shack/shopping_cart/cart.py
@@ -1,14 +1,14 @@
-
 class Cart():
     def __init__(self, request):
         self.session = request.session
 
-        #Get the current session key if it exists
+        # Get the current session key if it exists
         cart = self.session.get('session_key')
 
-        #If user is new, no session key! Create One!
-        if 'session_key' not in request.session:
-            cart = self.session['session_key'] = {}
+        # If 'session_key' doesn't exist or isn't a dictionary, initialize it as an empty dictionary
+        if not isinstance(cart, dict):
+            cart = {}
+            self.session['session_key'] = cart
 
-        #Make sure cart is available on all pages of the site
+        # Make sure cart is available on all pages of the site
         self.cart = cart
--- The_Crep_Shack/shopping_cart/context_processors.py
+++ The_Crep_Shack/shopping_cart/context_processors.py
@@ -3,4 +3,4 @@ from .cart import Cart
 #Create context processor so our cart can work on all pages of the site
 def cart(request):
     #Return the default data from out Cart
-    return{'cart:', Cart(request)}
+    return{'cart:': Cart(request)}
